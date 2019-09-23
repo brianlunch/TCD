507532c50579a574895e0d703824d0e90c4d200f
brianlunch <blynch5@tcd.ie>
2019-09-23 17:24:28 +0100

    Added First Test with a right heavy bonary tree

--- Third Year/Software Engineering/src/LCATest.java
+++ Third Year/Software Engineering/src/LCATest.java
@@ -27,5 +27,40 @@ public class LCATest {
         assertEquals( 2,tree.findLCA(5, 4));
         assertEquals( 1,tree.findLCA(4, 6));
         assertEquals( 1,tree.findLCA(2, 3));
+        assertEquals( 3,tree.findLCA(6, 7));
+    }
+
+    @Test
+    public void testTwo() {
+        LCA.BT_NoParentPtr_Solution1 tree = new LCA.BT_NoParentPtr_Solution1();
+        tree.root = new LCA.Node(1);
+        tree.root.left = new LCA.Node(2);
+        tree.root.right = new LCA.Node(3);
+        tree.root.right.left  = new LCA.Node(4);
+        tree.root.right.right  = new LCA.Node(5);
+        tree.root.right.left.left   = new LCA.Node(6);
+        tree.root.right.left .right  = new LCA.Node(7);
+        tree.root.right.right.left  = new LCA.Node(8);
+        tree.root.right.right.right  = new LCA.Node(9);
+
+        /*
+
+        SEARCH TREE VISUALISATION
+
+        	 1
+		   /   \
+		  2     3
+		      /   \
+		     4      5
+			/ \    / \
+		   6   7  8   9
+
+         */
+
+        assertEquals( 3,tree.findLCA(4, 5));
+        assertEquals( 4,tree.findLCA(4, 6));
+        assertEquals( 1,tree.findLCA(2, 3));
+        assertEquals( 4,tree.findLCA(6, 7));
+        assertEquals( 1,tree.findLCA(6, 2));
     }
 }
